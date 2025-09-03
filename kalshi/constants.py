4571830bb877938c1685f3b6d0276c714ceff579
Jake Stenger <35145360+jsteng19@users.noreply.github.com>
2025-09-03 16:43:45 -0700

    add demo url as default, use .env

--- kalshi/constants.py
+++ kalshi/constants.py
@@ -1,2 +1,13 @@
-BASE_URL = "https://api.elections.kalshi.com"
+DEMO_BASE_URL = "https://demo-api.kalshi.co"
+PROD_BASE_URL = "https://api.elections.kalshi.com"
 BASE_PATH = "/trade-api/v2"
+
+BASE_URL = DEMO_BASE_URL
+
+def use_demo():
+	pass
+
+def use_prod():
+	global BASE_URL
+	BASE_URL = PROD_BASE_URL 
+
